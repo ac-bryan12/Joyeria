95ce023b635192e08453035b384d9faf4257c199
avillao <villaoaron@gmail.com>
2022-01-14 01:04:25 -0500

    Se agregaron los formularios para crear y modificar usuarios, y se conectaron las peticiones con el servidor.

--- src/app/componentes/admin/list-products/list-products.component.ts
+++ src/app/componentes/admin/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PeticionesService } from 'src/app/services/requests/peticiones.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class ListProductsComponent implements OnInit {
 
   productos:any[]= []
 
-  constructor(private service: PeticionesService) { }
+  constructor(private service: PeticionesService,private router:Router,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.cargarProductos()
@@ -25,15 +26,32 @@ export class ListProductsComponent implements OnInit {
   }
 
   modificarProducto(producto:any){
-    //redirect
+    this.router.navigate(
+      ['../modify_product'],
+      { relativeTo:this.route,
+        queryParams:{
+          id: producto.id,
+          nombre: producto.nombre,
+          categoria_id: producto.categoria.id,
+          image_url: producto.image_url,
+          precio: producto.precio,
+          promocion: producto.promocion,
+          stock:producto.stock
+        }
+      })
   }
 
   eliminarProducto(id:any){
-    this.service.peticionDelete("http://localhost:8000/api/articulos").subscribe(res=>{
-
+    let resp = confirm(`¿Seguro que desea eliminar el producto con id=${id} ?`)
+    if(resp){
+      this.service.peticionDelete("http://localhost:8000/api/articulos/"+id).subscribe(res=>{
+      alert(res.msg)
+      window.location.reload()
     },err=>{
-      
+      alert(err.error.error)
     })
+    }
+    
   }
 
 }
